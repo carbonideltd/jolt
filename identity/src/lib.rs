8e181fed137f3dc3f11f1a11bfba364acf4fb428
Stewart Mackenzie <sjm@fractalide.com>
2019-07-16 21:03:41 +0800

    Problem: users might not be able to derive their address easily
    Solution: include the address into the ciphertext and print address

--- identity/src/lib.rs
+++ identity/src/lib.rs
@@ -36,6 +36,7 @@ pub struct Model {
     encrypted: String,
     decrypted: String,
     download_href: String,
+    completed: bool,
 }
 
 pub enum Msg {
@@ -57,6 +58,7 @@ impl Component for Model {
             encrypted: "".into(),
             decrypted: "".into(),
             download_href: "data:text/plain;charset=utf-8,".into(),
+            completed: false,
         }
     }
 
@@ -84,16 +86,17 @@ impl Renderable<Model> for Model {
                     oninput=|e| Msg::GotPassword(e.value)
                     placeholder="Password">
                 </input>
-                <nav class="menu">
-                    <button onclick=|_| Msg::Generate>{ "Generate" }</button>
-                </nav>
-                <p>{ &self.sk }</p>
-                <p>{ &self.pk }</p>
+                <button onclick=|_| Msg::Generate>{ "Generate" }</button>
                 <p>{ &self.address }</p>
-                <p>{ &self.encrypted }</p>
-                <p>{ &self.decrypted }</p>
-                <p>{ &self.download_href }</p>
-                <a download="filename.txt" href=self.download_href.clone()>{"text file"}</a>
+                {
+                    if self.completed == true {
+                        html! {
+                            <a download="jolt.key" href=self.download_href.clone()>{"download your jolt keys"}</a>
+                        }
+                    } else {
+                        html! { <a type="hidden" />  }
+                    }
+                }
             </div>
         }
     }
@@ -111,11 +114,14 @@ impl Model {
         let sk: SecretKey<Ed25519> = SecretKey::generate(&mut rng);
         self.sk = sk.to_bech32_str();
         let pk: PublicKey<Ed25519> = sk.to_public();
+        js! { console.log( @{ format!("sk length: {:?}", self.sk.len()) })};
         self.pk = pk.clone().to_bech32_str();
+        js! { console.log( @{ format!("pk length: {:?}", self.pk.len()) })};
         let address = chain_addr::Address(DISCRIMINATION, chain_addr::Kind::Single(pk.clone()));
         self.address = address::Address::from(address).to_string();
-        self.console.log("generate");
-        let crypto_material = format!("{}\n{}",sk.to_bech32_str(), pk.clone().to_bech32_str() );
+        js! { console.log( @{ format!("address length: {:?}", self.address.len()) })};
+        let crypto_material = format!("{}{}{}",sk.to_bech32_str(), pk.clone().to_bech32_str(), self.address);
+        // let crypto_material = format!("{}\n{}",sk.to_bech32_str(), pk.clone().to_bech32_str());
         let digest = encrypt(self.password.clone(), &crypto_material);
         self.encrypted = digest.clone();
         let cleartext = decrypt(self.password.clone(), digest.clone());
@@ -124,6 +130,7 @@ impl Model {
                 assert_eq!(&crypto_material[..], &ct[..]);
                 self.decrypted = ct;
                 self.download_href.push_str(&digest.clone().as_str());
+                self.completed = true;
             },
             None => {
                 panic!("Couldn't decrypt")
@@ -137,7 +144,7 @@ const SALT_SIZE: usize = 16;
 const NONCE_SIZE: usize = 12;
 const TAG_SIZE: usize = 16;
 const KEY_SIZE: usize = 32;
-const CLEARTEXT_SIZE: usize = 139;
+const CLEARTEXT_SIZE: usize = 201;
 const CIPHERTEXT_SIZE: usize = CLEARTEXT_SIZE;
 const DIGEST_SIZE: usize = SALT_SIZE + NONCE_SIZE + TAG_SIZE + CIPHERTEXT_SIZE;
 
@@ -146,7 +153,6 @@ type Key = [u8; KEY_SIZE];
 type Salt = [u8; SALT_SIZE];
 type Nonce = [u8; NONCE_SIZE];
 type Tag = [u8; TAG_SIZE];
-
 
 fn generate_nonce() -> Nonce {
     let mut buf: Nonce = [0u8; NONCE_SIZE];
@@ -179,7 +185,6 @@ fn encrypt(password: String, cleartext: &String) -> String {
     let mut tag: Tag = [0; TAG_SIZE];
     let cleartext: Vec<u8> = cleartext.as_bytes().to_vec();
     let mut ciphertext: Vec<u8> = vec!(0u8; CIPHERTEXT_SIZE);
-
     let mut cipher = ChaCha20Poly1305::new(&key, &nonce, &[]);
     cipher.encrypt(&cleartext, &mut ciphertext, &mut tag[..]);
     let mut digest: Vec<u8> = Vec::with_capacity(DIGEST_SIZE);
@@ -187,7 +192,7 @@ fn encrypt(password: String, cleartext: &String) -> String {
     digest.extend_from_slice(&nonce);
     digest.append(&mut ciphertext);
     digest.extend_from_slice(&tag);
-    js! { console.log( @{ format!("encrypted: {:?}", digest) })};
+    // js! { console.log( @{ format!("encrypted: {:?}", digest) })};
     encode(&digest)
 }
 
@@ -205,55 +210,9 @@ fn decrypt( password: String, digest : String) -> Option<String> {
 
     let mut decipher = ChaCha20Poly1305::new(&key[..], &nonce[..], &[]);
     if decipher.decrypt(&digest[0..len], &mut cleartext[..], &digest[len..]) {
-        js! { console.log( @{ format!("decrypted: {:?}", cleartext) })};
+        // js! { console.log( @{ format!("decrypted: {:?}", cleartext) })};
         Some(String::from_utf8(cleartext).unwrap())
     } else {
         None
     }
 }
-
-
-// fn test_vector2() {
-//     struct TestVector2 {
-//         key: [u8; 32],
-//         nonce: Vec<u8>,
-//         tag: [u8;16],
-//         plain_text: Vec<u8>,
-//         cipher_text: Vec<u8>,
-//         aad: Vec<u8>,
-//     };
-//     let v = TestVector2 {
-//         key: [0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f],
-//         nonce: vec!(0x07, 0x00, 0x00, 0x00, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47),
-//         tag: [0x1a, 0xe1, 0x0b, 0x59, 0x4f, 0x09, 0xe2, 0x6a, 0x7e, 0x90, 0x2e, 0xcb, 0xd0, 0x60, 0x06, 0x91],
-//         plain_text: vec!(0x4c, 0x61, 0x64, 0x69, 0x65, 0x73, 0x20, 0x61, 0x6e, 0x64, 0x20, 0x47, 0x65, 0x6e, 0x74, 0x6c, 0x65, 0x6d, 0x65, 0x6e, 0x20, 0x6f, 0x66, 0x20, 0x74, 0x68, 0x65, 0x20, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x20, 0x6f, 0x66, 0x20, 0x27, 0x39, 0x39, 0x3a, 0x20, 0x49, 0x66, 0x20, 0x49, 0x20, 0x63, 0x6f, 0x75, 0x6c, 0x64, 0x20, 0x6f, 0x66, 0x66, 0x65, 0x72, 0x20, 0x79, 0x6f, 0x75, 0x20, 0x6f, 0x6e, 0x6c, 0x79, 0x20, 0x6f, 0x6e, 0x65, 0x20, 0x74, 0x69, 0x70, 0x20, 0x66, 0x6f, 0x72, 0x20, 0x74, 0x68, 0x65, 0x20, 0x66, 0x75, 0x74, 0x75, 0x72, 0x65, 0x2c, 0x20, 0x73, 0x75, 0x6e, 0x73, 0x63, 0x72, 0x65, 0x65, 0x6e, 0x20, 0x77, 0x6f, 0x75, 0x6c, 0x64, 0x20, 0x62, 0x65, 0x20, 0x69, 0x74, 0x2e),
-//         cipher_text: vec!(0xd3, 0x1a, 0x8d, 0x34, 0x64, 0x8e, 0x60, 0xdb, 0x7b, 0x86, 0xaf, 0xbc, 0x53, 0xef, 0x7e, 0xc2, 0xa4, 0xad, 0xed, 0x51, 0x29, 0x6e, 0x08, 0xfe, 0xa9, 0xe2, 0xb5, 0xa7, 0x36, 0xee, 0x62, 0xd6, 0x3d, 0xbe, 0xa4, 0x5e, 0x8c, 0xa9, 0x67, 0x12, 0x82, 0xfa, 0xfb, 0x69, 0xda, 0x92, 0x72, 0x8b, 0x1a, 0x71, 0xde, 0x0a, 0x9e, 0x06, 0x0b, 0x29, 0x05, 0xd6, 0xa5, 0xb6, 0x7e, 0xcd, 0x3b, 0x36, 0x92, 0xdd, 0xbd, 0x7f, 0x2d, 0x77, 0x8b, 0x8c, 0x98, 0x03, 0xae, 0xe3, 0x28, 0x09, 0x1b, 0x58, 0xfa, 0xb3, 0x24, 0xe4, 0xfa, 0xd6, 0x75, 0x94, 0x55, 0x85, 0x80, 0x8b, 0x48, 0x31, 0xd7, 0xbc, 0x3f, 0xf4, 0xde, 0xf0, 0x8e, 0x4b, 0x7a, 0x9d, 0xe5, 0x76, 0xd2, 0x65, 0x86, 0xce, 0xc6, 0x4b, 0x61, 0x16),
-//         aad: vec!(0x50, 0x51, 0x52, 0x53, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7),
-//     };
-//     let mut tag = [0u8; 16];
-//     let mut ciphertext = vec!(0u8; v.cipher_text.len());
-//     let mut cipher = ChaCha20Poly1305::new(&v.key, &v.nonce, &v.aad);
-//     let mut decipher = cipher.clone();
-//     cipher.encrypt(&v.plain_text, &mut ciphertext, &mut tag[..]);
-//     assert_eq!(&ciphertext[..], &v.cipher_text[..]);
-//     assert_eq!(&tag[..], &v.tag[..]);
-//     let mut cipher_tag: Vec<u8> = Vec::with_capacity(ciphertext.len() + tag.len());
-//     js! { console.log( @{ format!("length of tag: {:?}", tag.len()) })};
-//     js! { console.log( @{ format!("length of plain_text: {:?}", v.plain_text.len()) })};
-//     js! { console.log( @{ format!("length of ciphertext: {:?}", ciphertext.len()) })};
-//     js! { console.log( @{ format!("length of cipher_tag: {:?}", cipher_tag.len()) })};
-//     cipher_tag.extend_from_slice(&tag);
-//     js! { console.log( @{ format!("length of cipher_tag1: {:?}", cipher_tag.len()) })};
-//     cipher_tag.append(&mut ciphertext);
-//     js! { console.log( @{ format!("cipher_tag: {:?}", cipher_tag) })};
-//     js! { console.log( @{ format!("length of cipher_tag2: {:?}", cipher_tag.len()) })};
-//     js! { console.log( @{ format!("calculated tag: {:?}", tag) })};
-//     js! { console.log( @{ format!("derived    tag: {:?}", v.tag) })};
-//     let (tag1, ciphertext1) = cipher_tag.split_at(16);
-//     js! { console.log( @{ format!("split tag:      {:?}", tag1) })};
-//     assert_eq!(&ciphertext1[..], &v.cipher_text[..]);
-//     assert_eq!(&tag1[..], &v.tag[..]);
-//     let mut output = vec!(0u8; v.plain_text.len());
-//     assert!(decipher.decrypt(&ciphertext1, &mut output, &tag1[..]), true);
-//     assert_eq!(&output[..], &v.plain_text[..]);
-// }
--- identity_cli/src/main.rs
+++ identity_cli/src/main.rs
@@ -5,12 +5,15 @@ use std::fs::File;
 use std::env;
 use cryptoxide::{chacha20poly1305::ChaCha20Poly1305, hmac::Hmac, pbkdf2::pbkdf2, sha2::Sha512};
 use std::{
-    io::Read,
+    io::{Read},
     iter::repeat,
 };
 use base64::decode;
 
 const PASSWORD_DERIVATION_ITERATIONS: u32 = 10_000;
+const SK_SIZE: usize = 69;
+const PK_SIZE: usize = 69;
+const ADDRESS_SIZE: usize = 63;
 const SALT_SIZE: usize = 16;
 const NONCE_SIZE: usize = 12;
 const TAG_SIZE: usize = 16;
@@ -20,15 +23,19 @@ pub type Password = [u8];
 type Key = [u8; KEY_SIZE];
 type Salt = [u8; SALT_SIZE];
 type Nonce = [u8; NONCE_SIZE];
+type SK = [u8; SK_SIZE];
+type PK = [u8; PK_SIZE];
+type Address = [u8; ADDRESS_SIZE];
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let password = &args[1];
-    println!("Jolt key path: {}.", args[2]);
+    println!("key path:  {}", args[2]);
     let mut file = File::open(args[2].clone()).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    decrypt(password.to_string(), contents);
+    decrypt(password.to_string(), contents)?;
+    Ok(())
 }
 
 fn password_to_key(password: &Password, salt: Salt, key: &mut Key) {
@@ -36,25 +43,33 @@ fn password_to_key(password: &Password, salt: Salt, key: &mut Key) {
     pbkdf2(&mut mac, &salt[..], PASSWORD_DERIVATION_ITERATIONS, key);
 }
 
-fn decrypt( password: String, digest : String) {
-    let mut digest = &decode(&digest).unwrap()[..];
+fn decrypt( password: String, digest : String) -> std::io::Result<()>{
+    let digest = decode(&digest).unwrap();
+    let mut digest = &digest[..];
     let mut salt: Salt = [0; SALT_SIZE];
     let mut nonce: Nonce = [0; NONCE_SIZE];
     let mut key: Key = [0; KEY_SIZE];
+    let mut address: Address = [0; ADDRESS_SIZE];
+    let mut sk: SK = [0; SK_SIZE];
+    let mut pk: PK = [0; PK_SIZE];
     let len = digest.len() - TAG_SIZE - SALT_SIZE - NONCE_SIZE;
     let mut cleartext: Vec<u8> = repeat(0).take(len).collect();
-
     digest.read_exact(&mut salt[..]).unwrap();
     digest.read_exact(&mut nonce[..]).unwrap();
     password_to_key(password.as_bytes(), salt, &mut key);
 
     let mut decipher = ChaCha20Poly1305::new(&key[..], &nonce[..], &[]);
     if decipher.decrypt(&digest[0..len], &mut cleartext[..], &digest[len..]) {
-        let cleartext_string = String::from_utf8(cleartext).unwrap();
-        let (sk, rest) = cleartext_string.split_at(cleartext_string.len() / 2);
-        let (_, pk) = rest.split_at(1);
-        println!("secretkey: {}\npublickey: {}",sk ,pk);
+        let mut cleartext = &cleartext[..];
+        cleartext.read_exact(&mut sk[..]).unwrap();
+        cleartext.read_exact(&mut pk[..]).unwrap();
+        cleartext.read_exact(&mut address[..]).unwrap();
+        println!("secretkey: {}\npublickey: {}\naddress:   {}"
+        , String::from_utf8(sk.to_vec()).unwrap()
+        , String::from_utf8(pk.to_vec()).unwrap()
+        , String::from_utf8(address.to_vec()).unwrap());
     } else {
         println!("Couldn't decrypt");
     }
+    Ok(())
 }
